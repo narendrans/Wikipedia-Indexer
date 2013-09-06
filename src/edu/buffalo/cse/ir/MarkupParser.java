75a745cc3234d07625ec97b1b46a7b7eff3004c1
tsaschik <tsaschik@buffalo.edu>
2013-09-06 11:43:47 -0400

    first comment

--- src/edu/buffalo/cse/ir/MarkupParser.java
+++ src/edu/buffalo/cse/ir/MarkupParser.java
@@ -24,6 +24,7 @@ public class MarkupParser {
 			category.add(matcher.group());
 		}
 
+		// testing a change to class
 		// Prints the number of elements in category list
 		System.out.println(category.size());
 
