0f229e1ccc0f39822e2ae87f19a87068abf29c64
Naren <naren.kernel@gmail.com>
2013-09-08 01:17:30 -0400

    added parsing code for text formatting, sections and list items

--- src/edu/buffalo/cse/ir/wikiindexer/wikipedia/WikipediaParser.java
+++ src/edu/buffalo/cse/ir/wikiindexer/wikipedia/WikipediaParser.java
@@ -3,83 +3,121 @@
  */
 package edu.buffalo.cse.ir.wikiindexer.wikipedia;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * @author nikhillo
- * This class implements Wikipedia markup processing.
- * Wikipedia markup details are presented here: http://en.wikipedia.org/wiki/Help:Wiki_markup
- * It is expected that all methods marked "todo" will be implemented by students.
- * All methods are static as the class is not expected to maintain any state.
+ * @author nikhillo This class implements Wikipedia markup processing. Wikipedia
+ *         markup details are presented here:
+ *         http://en.wikipedia.org/wiki/Help:Wiki_markup It is expected that all
+ *         methods marked "todo" will be implemented by students. All methods
+ *         are static as the class is not expected to maintain any state.
  */
 public class WikipediaParser {
 	/* TODO */
 	/**
-	 * Method to parse section titles or headings.
-	 * Refer: http://en.wikipedia.org/wiki/Help:Wiki_markup#Sections
-	 * @param titleStr: The string to be parsed
+	 * Method to parse section titles or headings. Refer:
+	 * http://en.wikipedia.org/wiki/Help:Wiki_markup#Sections
+	 * 
+	 * @param titleStr
+	 *            : The string to be parsed
 	 * @return The parsed string with the markup removed
 	 */
 	public static String parseSectionTitle(String titleStr) {
-		return null;
+		if(titleStr==null)
+			return null;
+		else if(titleStr.isEmpty())
+			return "";
+		Pattern regex = Pattern.compile("=+ (.+?) =+");
+		Matcher matcher = regex.matcher(titleStr);
+		matcher.find();
+		return matcher.group(1);
 	}
-	
+
 	/* TODO */
 	/**
 	 * Method to parse list items (ordered, unordered and definition lists).
 	 * Refer: http://en.wikipedia.org/wiki/Help:Wiki_markup#Lists
-	 * @param itemText: The string to be parsed
+	 * 
+	 * @param itemText
+	 *            : The string to be parsed
 	 * @return The parsed string with markup removed
 	 */
 	public static String parseListItem(String itemText) {
-		return null;
+		if(itemText==null)
+			return null;
+		else if (itemText.isEmpty())
+			return "";
+		Pattern regex = Pattern.compile("(?:\\*+|#+|:+) (.+)");
+		Matcher matcher = regex.matcher(itemText);
+		matcher.find();
+		return matcher.group(1);
 	}
-	
+
 	/* TODO */
 	/**
-	 * Method to parse text formatting: bold and italics.
-	 * Refer: http://en.wikipedia.org/wiki/Help:Wiki_markup#Text_formatting first point
-	 * @param text: The text to be parsed
+	 * Method to parse text formatting: bold and italics. Refer:
+	 * http://en.wikipedia.org/wiki/Help:Wiki_markup#Text_formatting first point
+	 * 
+	 * @param text
+	 *            : The text to be parsed
 	 * @return The parsed text with the markup removed
 	 */
 	public static String parseTextFormatting(String text) {
-		return null;
+		if(text==null)
+			return null;
+		else if(text.isEmpty())
+			return "";
+		return text.replaceAll("'+", "");
+		
 	}
-	
+
 	/* TODO */
 	/**
-	 * Method to parse *any* HTML style tags like: <xyz ...> </xyz>
-	 * For most cases, simply removing the tags should work.
-	 * @param text: The text to be parsed
+	 * Method to parse *any* HTML style tags like: <xyz ...> </xyz> For most
+	 * cases, simply removing the tags should work.
+	 * 
+	 * @param text
+	 *            : The text to be parsed
 	 * @return The parsed text with the markup removed.
 	 */
 	public static String parseTagFormatting(String text) {
-		return null;
+		if(text==null)
+			return null;
+		else if(text.isEmpty())
+			return "";
+		text = text.replaceAll("<[^>]*>", "");
+		//text = text.replaceAll("<[^>]*>", "");
+		return text;
+		
 	}
-	
+
 	/* TODO */
 	/**
-	 * Method to parse wikipedia templates. These are *any* {{xyz}} tags
-	 * For most cases, simply removing the tags should work.
-	 * @param text: The text to be parsed
+	 * Method to parse wikipedia templates. These are *any* {{xyz}} tags For
+	 * most cases, simply removing the tags should work.
+	 * 
+	 * @param text
+	 *            : The text to be parsed
 	 * @return The parsed text with the markup removed
 	 */
 	public static String parseTemplates(String text) {
 		return null;
 	}
-	
-	
+
 	/* TODO */
 	/**
-	 * Method to parse links and URLs.
-	 * Refer: http://en.wikipedia.org/wiki/Help:Wiki_markup#Links_and_URLs
-	 * @param text: The text to be parsed
-	 * @return An array containing two elements as follows - 
-	 *  The 0th element is the parsed text as visible to the user on the page
-	 *  The 1st element is the link url
+	 * Method to parse links and URLs. Refer:
+	 * http://en.wikipedia.org/wiki/Help:Wiki_markup#Links_and_URLs
+	 * 
+	 * @param text
+	 *            : The text to be parsed
+	 * @return An array containing two elements as follows - The 0th element is
+	 *         the parsed text as visible to the user on the page The 1st
+	 *         element is the link url
 	 */
 	public static String[] parseLinks(String text) {
 		return null;
 	}
-	
-	
-	
+
 }
