307cc78321643f67ac3a61a842d74fd7a7a66d86
Naren <naren.kernel@gmail.com>
2013-09-16 19:34:59 -0400

    whitespace rule tokenizer

---
+++ src/edu/buffalo/cse/ir/wikiindexer/tokenizer/rules/WhiteSpaceRule.java
@@ -0,0 +1,21 @@
+package edu.buffalo.cse.ir.wikiindexer.tokenizer.rules;
+
+import edu.buffalo.cse.ir.wikiindexer.tokenizer.TokenStream;
+import edu.buffalo.cse.ir.wikiindexer.tokenizer.TokenizerException;
+import edu.buffalo.cse.ir.wikiindexer.tokenizer.rules.TokenizerRule.RULENAMES;
+
+@RuleClass(className = RULENAMES.WHITESPACE)
+public class WhiteSpaceRule implements TokenizerRule {
+
+	public String[] removeWhiteSpace(String sb) {
+		sb = sb.replaceAll(" +", " ");
+		return sb.split(" ");
+	}
+
+	@Override
+	public void apply(TokenStream stream) throws TokenizerException {
+		if (stream != null)
+			stream.set(removeWhiteSpace(stream.stringBuilder.toString()));
+	}
+
+}
