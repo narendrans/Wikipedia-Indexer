41f9f32ec76d935acfc2794c87cd9f17d5d48f60
Naren <naren.kernel@gmail.com>
2013-09-23 12:15:54 -0400

    punctuation rule tests passed

--- src/edu/buffalo/cse/ir/wikiindexer/tokenizer/rules/PunctuationRule.java
+++ src/edu/buffalo/cse/ir/wikiindexer/tokenizer/rules/PunctuationRule.java
@@ -8,7 +8,6 @@ import java.util.regex.Pattern;
 
 import edu.buffalo.cse.ir.wikiindexer.tokenizer.TokenStream;
 import edu.buffalo.cse.ir.wikiindexer.tokenizer.TokenizerException;
-import edu.buffalo.cse.ir.wikiindexer.tokenizer.rules.EnglishStemmer.Stemmer;
 import edu.buffalo.cse.ir.wikiindexer.tokenizer.rules.TokenizerRule.RULENAMES;
 
 /**
@@ -17,7 +16,6 @@ import edu.buffalo.cse.ir.wikiindexer.tokenizer.rules.TokenizerRule.RULENAMES;
  */
 @RuleClass(className = RULENAMES.PUNCTUATION)
 public class PunctuationRule implements TokenizerRule {
-
 	// Local tester main
 	public static void main(String[] args) {
 		System.out.println(removePunctuation("tes.t.!"));
@@ -25,15 +23,21 @@ public class PunctuationRule implements TokenizerRule {
 
 	private static String removePunctuation(String text) {
 
-		if (text.contains("?") || text.contains("!")) {
-			text = text.replaceAll("\\?|\\!", "");
-		}
-		if (text.matches(".+\\.")) {
-			Pattern regex = Pattern.compile("(.+)\\.");
+		text = text.replaceAll("\\. |\\? |\\! ", " ");
+
+		if (text.matches("(\\d+\\.\\d+\\.\\d+\\.\\d+)")) {
+
+			return text;
+		} else if (text.matches("(\\d+\\.\\d+\\.\\d+\\.\\d+).+")) {
+			Pattern regex = Pattern.compile("(\\d+\\.\\d+\\.\\d+\\.\\d+).+");
 			Matcher matcher = regex.matcher(text);
 			matcher.find();
 			text = matcher.group(1);
+			return text;
 		}
+		else if (text.matches(".+\\?|.+\\!|.+\\."))
+			text = text.replaceAll("\\.|\\?|\\!", "");
+
 		return text;
 
 	}
