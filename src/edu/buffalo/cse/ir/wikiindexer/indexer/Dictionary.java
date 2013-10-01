10cb10028d2910de04d5d482a0d5e133ebb969e4
Naren <naren.kernel@gmail.com>
2013-09-30 23:10:06 -0400

    dictionary

--- src/edu/buffalo/cse/ir/wikiindexer/indexer/Dictionary.java
+++ src/edu/buffalo/cse/ir/wikiindexer/indexer/Dictionary.java
@@ -3,19 +3,55 @@
  */
 package edu.buffalo.cse.ir.wikiindexer.indexer;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
- * @author nikhillo
- * An abstract class that represents a dictionary object for a given index
+ * @author nikhillo An abstract class that represents a dictionary object for a
+ *         given index
  */
 public abstract class Dictionary implements Writeable {
-	public Dictionary (Properties props, INDEXFIELD field) {
-		//TODO Implement this method
+	Map<String, Integer> myMap;
+
+	Properties props;
+	INDEXFIELD fld;
+
+	public Map<String, Integer> getMyMap() {
+		return myMap;
 	}
-	
-	/* (non-Javadoc)
+
+	public void setMyMap(Map<String, Integer> myMap) {
+		this.myMap = myMap;
+	}
+
+	public Properties getProps() {
+		return props;
+	}
+
+	public void setProps(Properties props) {
+		this.props = props;
+	}
+
+	public INDEXFIELD getFld() {
+		return fld;
+	}
+
+	public void setFld(INDEXFIELD fld) {
+		this.fld = fld;
+	}
+
+	public Dictionary(Properties props, INDEXFIELD field) {
+		this.props = props;
+		this.fld = field;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see edu.buffalo.cse.ir.wikiindexer.indexer.Writeable#writeToDisk()
 	 */
 	public void writeToDisk() throws IndexerException {
@@ -23,45 +59,61 @@ public abstract class Dictionary implements Writeable {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see edu.buffalo.cse.ir.wikiindexer.indexer.Writeable#cleanUp()
 	 */
 	public void cleanUp() {
 		// TODO Implement this method
 
 	}
-	
+
 	/**
-	 * Method to check if the given value exists in the dictionary or not
-	 * Unlike the subclassed lookup methods, it only checks if the value exists
-	 * and does not change the underlying data structure
-	 * @param value: The value to be looked up
+	 * Method to check if the given value exists in the dictionary or not Unlike
+	 * the subclassed lookup methods, it only checks if the value exists and
+	 * does not change the underlying data structure
+	 * 
+	 * @param value
+	 *            : The value to be looked up
 	 * @return true if found, false otherwise
 	 */
 	public boolean exists(String value) {
-		//TODO Implement this method
-		return false;
+		if(myMap.containsKey(value))
+			return true;
+		else
+			return false;
 	}
-	
+
 	/**
-	 * MEthod to lookup a given string from the dictionary.
-	 * The query string can be an exact match or have wild cards (* and ?)
-	 * Must be implemented ONLY AS A BONUS
-	 * @param queryStr: The query string to be searched
+	 * MEthod to lookup a given string from the dictionary. The query string can
+	 * be an exact match or have wild cards (* and ?) Must be implemented ONLY
+	 * AS A BONUS
+	 * 
+	 * @param queryStr
+	 *            : The query string to be searched
 	 * @return A collection of ordered strings enumerating all matches if found
-	 * null if no match is found
+	 *         null if no match is found
 	 */
 	public Collection<String> query(String queryStr) {
-		//TODO: Implement this method (FOR A BONUS)
-		return null;
+		List<String> s = new ArrayList<String>();
+		queryStr = queryStr.replaceAll("\\*",".*");
+		for (Map.Entry<String, Integer> entry : myMap.entrySet()) {
+			if (entry.getKey().matches(queryStr))
+				s.add(entry.getKey());
+		}
+		if (s.isEmpty())
+			return null;
+		else
+			return s;
 	}
-	
+
 	/**
 	 * Method to get the total number of terms in the dictionary
+	 * 
 	 * @return The size of the dictionary
 	 */
 	public int getTotalTerms() {
-		//TODO: Implement this method
-		return -1;
+		return myMap.size();
 	}
 }
--- src/edu/buffalo/cse/ir/wikiindexer/indexer/LocalDictionary.java
+++ src/edu/buffalo/cse/ir/wikiindexer/indexer/LocalDictionary.java
@@ -3,37 +3,59 @@
  */
 package edu.buffalo.cse.ir.wikiindexer.indexer;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Random;
 
 /**
- * @author nikhillo
- * This class represents a subclass of a Dictionary class that is
- * local to a single thread. All methods in this class are
- * assumed thread safe for the same reason.
+ * @author nikhillo This class represents a subclass of a Dictionary class that
+ *         is local to a single thread. All methods in this class are assumed
+ *         thread safe for the same reason.
  */
 public class LocalDictionary extends Dictionary {
-	
+	Map<String, Integer> myMap;
+
+	Properties props;
+	INDEXFIELD fld;
+
 	/**
 	 * Public default constructor
-	 * @param props: The properties file
-	 * @param field: The field being indexed by this dictionary
+	 * 
+	 * @param props
+	 *            : The properties file
+	 * @param field
+	 *            : The field being indexed by this dictionary
 	 */
 	public LocalDictionary(Properties props, INDEXFIELD field) {
 		super(props, field);
-		// TODO Auto-generated constructor stub
+		this.props = props;
+		this.fld = field;
+		myMap = new HashMap<String, Integer>();
+		super.setMyMap(myMap);
+
 	}
-	
+
 	/**
-	 * Method to lookup and possibly add a mapping for the given value
-	 * in the dictionary. The class should first try and find the given
-	 * value within its dictionary. If found, it should return its
-	 * id (Or hash value). If not found, it should create an entry and
-	 * return the newly created id.
-	 * @param value: The value to be looked up
+	 * Method to lookup and possibly add a mapping for the given value in the
+	 * dictionary. The class should first try and find the given value within
+	 * its dictionary. If found, it should return its id (Or hash value). If not
+	 * found, it should create an entry and return the newly created id.
+	 * 
+	 * @param value
+	 *            : The value to be looked up
 	 * @return The id as explained above.
 	 */
 	public int lookup(String value) {
-		//TODO Implement this method
-		return -1;
+		if (myMap.containsKey(value))
+			return myMap.get(value);
+		else {
+			Random r = new Random();
+			int rnd = r.nextInt();
+			if(rnd<0)
+				rnd = Math.abs(rnd);
+			myMap.put(value, rnd);
+			return rnd;
+		}
 	}
 }
